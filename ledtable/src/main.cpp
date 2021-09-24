45da18cf407841f4716a42d70d3cecd0321886eb
Matic Babnik <matic.babnik@outlook.com>
2021-09-24 08:43:57 +0200

    Now works with mpv-ledtable

--- ledtable/src/main.cpp
+++ ledtable/src/main.cpp
@@ -12,17 +12,20 @@ StripDisplay Display(20, 20);
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(576000);
   Serial.setTimeout(1000);
   Serial.println("starting");
   FastLED.addLeds<WS2812B, 17, GRB>(Display.data, Display.height * Display.width);
   FastLED.setBrightness(100);
 }
-//valid frame = !FR<50bytes>!EF
-byte seq_beg[3] = {'!', 'F', 'R'};
+//valid frame = !BF<600bytes>!EF
+byte seq_beg[3] = {'!', 'B', 'F'};
 byte seq_end[3] = {'!', 'E', 'F'};
 
-byte frame[53];
+byte frame[603];
+#define SEQ_LEN 3
+#define FRAME_SIZE 600
+#define FRAME_SIZE_E (SEQ_LEN + FRAME_SIZE)
 
 void loop()
 {
@@ -30,7 +33,7 @@ void loop()
   {
   }
   int seq_beg_index = 0;
-  while (seq_beg_index < 3)
+  while (seq_beg_index < SEQ_LEN)
   { //read a valid header
     byte r = Serial.read();
     if (r == seq_beg[seq_beg_index])
@@ -39,32 +42,59 @@ void loop()
     }
   }
 
-  size_t read_len = Serial.readBytes(frame, 53);
-  if (read_len != 53)
+  size_t read_len = Serial.readBytes(frame, FRAME_SIZE_E);
+  if (read_len != FRAME_SIZE_E)
     return;
 
-  for (int i = 0; i < 3; i++)
-    if (frame[50 + i] != seq_end[i])
+  for (int i = 0; i < SEQ_LEN; i++)
+    if (frame[FRAME_SIZE + i] != seq_end[i])
       return;
 
   const int w = 20, h = 20;
-  int x = 0, y = 0;
-  
+
   Serial.println("Frame");
   Display.clear();
-  for (int i = 0; i < 50; i++)
+  int x = 0, y = 0;
+  byte c_r, c_g, c_b;
+  enum
   {
-    for (int j = 0; j < 8; j++)
+    CRG,
+    CBR,
+    CGB
+  } state = CRG;
+  for (int i = 0; i < FRAME_SIZE_E; i++)
+  {
+    byte cur_byte = frame[i];
+    switch (state)
     {
-      byte cur = 0b10000000 & (frame[i] << j);
-      Display.drawPoint(x, y, cur > 0 ? CRGB::White : CRGB::Black);
+    case CRG:
+      c_r = cur_byte & 0xf0;
+      c_g = (cur_byte & 0x0f) << 4;
+      state = CBR;
+      break;
+    case CBR:
+      c_b = cur_byte & 0xf0;
+      Display.drawPoint(x, y, CRGB(c_r, c_g, c_b));
+      c_r = (cur_byte & 0x0f) << 4;
+      state = CGB;
       x++;
-      if (x == 20)
-      {
-        x = 0;
-        y++;
-      }
+
+      break;
+    case CGB:
+      c_g = cur_byte & 0xf0;
+      c_b = (cur_byte & 0x0f) << 4;
+      Display.drawPoint(x, y, CRGB(c_r, c_g, c_b));
+      state = CRG;
+      x++;
+
+      break;
+    }
+    if (x == 20)
+    {
+      x = 0;
+      y++;
     }
   }
+
   FastLED.show();
 }
